2e56f316a9a82c829cd610ca1e86d7e3371f4ff2
Dorota Szymańska <dorotaszymanska.official@gmail.com>
2025-07-28 11:28:38 +0200

    snake corrected

--- snake.py
+++ snake.py
@@ -8,13 +8,14 @@ BLACK = (0, 0, 0)
 RANGE_X = 1200
 RANGE_Y = 700
 
-FOOD_SIZE = 8
+FOOD_SIZE = 20
+BLOCK_SIZE = 20
 
 snake_x = 400.0
 snake_y = 400.0
 
-snake_width = 8
-snake_height = 8
+snake_width = 20
+snake_height = 20
 
 change_x = 0
 change_y = 0
@@ -23,6 +24,10 @@ running = True
 
 food_x = random.randint(1, RANGE_X - FOOD_SIZE)
 food_y = random.randint(1, RANGE_Y - FOOD_SIZE)
+
+clock = pygame.time.Clock()
+
+direction = "right"
 
 pygame.init()
 
@@ -33,57 +38,48 @@ screen = pygame.display.set_mode((RANGE_X, RANGE_Y))
 pygame.display.set_caption("Snake")
 
 while running:
-
+    clock.tick(60)
     screen.fill(BLACK)
+
+    for x in range(0, RANGE_X, BLOCK_SIZE):
+        for y in range(0, RANGE_Y, BLOCK_SIZE):
+            rect = pygame.Rect(x, y, BLOCK_SIZE, BLOCK_SIZE)
+            pygame.draw.rect(screen, WHITE, rect, 1)
+
+    keys = pygame.key.get_pressed()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    keys = pygame.key.get_pressed()
+    if keys[pygame.K_UP] and direction != "down":
+        direction = "up"
+        change_x = 0
+        change_y = -BLOCK_SIZE / 5
+    elif keys[pygame.K_DOWN] and direction != "up":
+        direction = "down"
+        change_x = 0
+        change_y = BLOCK_SIZE / 5
+    elif keys[pygame.K_LEFT] and direction != "right":
+        direction = "left"
+        change_x = -BLOCK_SIZE / 5
+        change_y = 0
+    elif keys[pygame.K_RIGHT] and direction != "left":
+        direction = "right"
+        change_x = BLOCK_SIZE / 5
+        change_y = 0
 
-    if snake_y <= 0:
-        snake_y = 0
-        change_y = RANGE_Y - snake_height
-    if snake_y >= RANGE_Y - snake_height:
-        snake_y = 0
-        change_y = 0
-    if snake_x <= 0:
-        snake_x = RANGE_X - snake_width
-        change_x = 0
-    if snake_x >= RANGE_X - snake_width:
-        snake_x = 0
-        change_x = 0
+    if snake_y <= 0 or snake_y >= RANGE_Y - snake_height:
+        change_y *= -1
+    if snake_x <= 0 or snake_x >= RANGE_X - snake_width:
+        change_x *= -1
 
-    if keys[pygame.K_UP]:
-        change_x = 0
-        change_y = -0.4
-    elif keys[pygame.K_DOWN]:
-        change_x = 0
-        change_y = 0.4
-    elif keys[pygame.K_LEFT]:
-        change_x = -0.4
-        change_y = 0
-    elif keys[pygame.K_RIGHT]:
-        change_x = 0.4
-        change_y = 0
+    snake_x += change_x
+    snake_y += change_y
 
     head = []
     head.append(snake_x)
     head.append(snake_y)
-
-    for segment in snake_list[:-1]:
-        if (
-            (keys[pygame.K_UP] and keys[pygame.K_DOWN])
-            or (keys[pygame.K_LEFT] and keys[pygame.K_RIGHT])
-            and (segment == head)
-        ):
-            change_x = 0.25
-            change_y = 0.25
-            # pass
-        else:
-            if segment == head:
-                running = False
 
     pygame.draw.rect(screen, WHITE, (food_x, food_y, FOOD_SIZE, FOOD_SIZE))
 
@@ -92,8 +88,11 @@ while running:
     if len(snake_list) > snake_length:
         del snake_list[0]
 
-    snake_x += change_x
-    snake_y += change_y
+    if len(snake_list) > 2 and snake_list[0] == snake_list[-1]:
+        FONT = pygame.font.Font(None, 36)
+        END_MESSAGE = FONT.render("Przegrana", True, RED)
+        screen.blit(END_MESSAGE, END_MESSAGE.get_rect())
+        running = False
 
     for segment in snake_list:
         pygame.draw.rect(
@@ -107,6 +106,6 @@ while running:
     if snake_rect.colliderect(food_rect):
         food_x = random.randint(1, RANGE_X)
         food_y = random.randint(1, RANGE_Y)
-        snake_length += 50
+        snake_length += 20
 
 pygame.quit()
